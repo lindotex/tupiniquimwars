f4079a8f77371bc71eb51d17e3ec7b18b77b8578
Alisson Lindote Novais <lindoten@gmail.com>
2025-02-03 12:05:17 +0000

    update: organized fulders and started state machine

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -1,30 +1,89 @@
 extends CharacterBody2D
+@onready var patrol_points: Node = $patrol_points
+@onready var animated_sprite2D = $AnimatedSprite2D
+@onready var timer = $Timer
 
-@export var GRAVITY: int = 700
-@export var speed: int = 100
-var direction: Vector2 = Vector2.LEFT
+@export var gravity: int = 700
+@export var speed: int = 2000
+
+var direction : Vector2
+var number_of_points: int
+var point_positions: Array[Vector2]
+var current_point
+var current_point_position: int
+var can_walk :bool = false
 
 enum State {Idle, Walk}
 var current_state = State
 
 func _ready():
+	# Patrol Points
+	if patrol_points != null:
+		number_of_points = patrol_points.get_children().size()
+		for point in patrol_points.get_children():
+			point_positions.append(point.global_position)
+		current_point = point_positions[current_point_position]
+	else:
+		print("No patrol Points associated")
+	# Main State 
 	current_state = State.Idle
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta) -> void:
+	# Verifying Gravity
 	enemy_gravity(delta)
+	animation()
+	
+	#Verifying which state the character is:
 	enemy_idle(delta)
-
+	enemy_walk(delta)
+	
+	# Move character
 	move_and_slide()
 
-
+# Setting the gravity
 func enemy_gravity(delta:float):
-	velocity.y = GRAVITY * delta
+	velocity.y = gravity * delta
 
-# Idle
+# Idle State
 func enemy_idle(delta:float):
-	velocity.x = move_toward(velocity.x, 0, speed * delta)
-	current_state = State.Idle
+	if !can_walk:
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		current_state = State.Idle
+		
+# Walk State
+func enemy_walk(delta):
+	if !can_walk:
+		return
+	
+	# Patrol function
+	if abs(position.x - current_point.x) > 0.5:
+		velocity.x = direction.x * speed * delta
+		current_state = State.Walk
+	else:
+		current_point_position += 1
+	
+	# Condition to not go Out of Bounds of numbers (2) after turning:
+	if current_point_position >= number_of_points:
+		current_point_position = 0
+	
+	current_point = point_positions[current_point_position]
+	
+	# Changes the direction whenever it is closer to an point in patrol points.
+	if current_point.x > position.x:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.LEFT
+	animated_sprite2D.flip_h = direction.x<0
+	can_walk = false
+	timer.start()
+	
+	
+func animation():
+	if current_state == State.Idle:
+		animated_sprite2D.play("idle")
+	if current_state == State.Walk:
+		animated_sprite2D.play("walk")
 
-func enemy_walk(delta:float):
-	velocity.x = direction * GRAVITY * delta
-	current_state = State.walk
+
+func _on_timer_timeout() -> void:
+	can_walk = true
--- scripts/hero/player.gd
+++ scripts/hero/player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 @onready var animated_sprite_2d = $Sprite2D
 
 @export var GRAVITY = 1000
-@export var walking_speed: float = 300
-@export var JUMP_FORCE: int = 500
-@export var JUMP_HORIZONTAL: int = 100
+@export var walking_speed: float = 2000
+@export var Jump_y_force: int = 500
+@export var Jump_x_force: int = 100
 @export var highscore: int = 0
 var RUNNING_SPEED = walking_speed * 1.5
 
@@ -19,6 +19,7 @@ func _ready():
 	pass
 
 func _physics_process(delta: float)->void:
+	player_animations()
 	player_falling(delta)
 	player_idle(delta)
 	player_walk(delta)
@@ -27,7 +28,11 @@ func _physics_process(delta: float)->void:
 	print("Current State:", State.keys()[current_state])
 	
 	move_and_slide()
-
+	
+func input_movement():
+	var direction: float = Input.get_axis("left", "right")
+	return direction
+	
 func player_falling(delta: float):
 	if !is_on_floor() and !player_jump(delta):
 		velocity.y += GRAVITY * delta
@@ -36,14 +41,16 @@ func player_falling(delta: float):
 func player_jump(delta: float):
 	var jumping = Input.is_action_pressed("jump")
 	if is_on_floor() and jumping:
-		velocity.y += -JUMP_FORCE
+		velocity.y += -Jump_y_force
 		current_state = State.Jump
 	if !is_on_floor() and current_state == State.Jump:
 		var direction = input_movement()
-		velocity.x += direction * JUMP_HORIZONTAL * delta
+		velocity.x += direction * Jump_x_force * delta
 
 func player_idle(delta: float):
-	if is_on_floor():
+	var direction = input_movement()
+	var movement = direction * delta
+	if is_on_floor() and !movement:
 		current_state = State.Idle
 
 func player_walk(delta: float):
@@ -56,6 +63,24 @@ func player_walk(delta: float):
 		current_state = State.Run
 		animated_sprite_2d.flip_h = false if direction > 0 else true
 
+#func player_death()->void:
+	#var player_death_effect_instance = player_death_effect.initiate() as Node2D
+	#player_death_effect_instance.global_position = global_position
+	#get_parent().add_child(player_death_effect_instance)
+	#queue_free()
+#
+#func _on_hurtbox_body_entered(body: Node2D)->void:
+	#if body.is_in_group("Enemy"):
+		#print("Enemy entered", body.damage_amount)
+		#var tween = get_tree().create_tween()
+		#tween.tween_property(animated_sprite_2d, "material:shader_parameter/enabled", true,0)
+		#tween.tween_property(animated_sprite_2d, "material:shader_parameter/enabled", false,0.2)
+		#
+		##hit_animation_player.play("hurt")
+		##HealthManager.decrease_health(body.damage_amount)
+	##if HealthManager.current_health == 0:
+		##player_death()
+
 func player_animations():
 	if current_state == State.Walk:
 		animated_sprite_2d.play("walk")
@@ -63,26 +88,3 @@ func player_animations():
 		animated_sprite_2d.play("idle")
 	elif current_state == State.Jump:
 		animated_sprite_2d.play("jump")
-
-func input_movement():
-	var direction: float = Input.get_axis("left", "right")
-	return direction
-
-func player_death()->void:
-	var player_death_effect_instance = player_death_effect.initiate() as Node2D
-	player_death_effect_instance.global_position = global_position
-	get_parent().add_child(player_death_effect_instance)
-	queue_free()
-
-func _on_hurtbox_body_entered(body: Node2D)->void:
-	if body.is_in_group("Enemy"):
-		print("Enemy entered", body.damage_amount)
-		var tween = get_tree().create_tween()
-		tween.tween_property(animated_sprite_2d, "material:shader_parameter/enabled", true,0)
-		tween.tween_property(animated_sprite_2d, "material:shader_parameter/enabled", false,0.2)
-		
-		#hit_animation_player.play("hurt")
-		#HealthManager.decrease_health(body.damage_amount)
-	#if HealthManager.current_health == 0:
-		#player_death()
-		
